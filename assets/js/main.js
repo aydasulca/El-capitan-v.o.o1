8ce9e23faaed0f382b46152b1e98511e9312fc1f
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-03-06 18:25:51 -0500

    botonnofunciona

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var mostrar= function sprint(texto1, texto2){
+  var text1 = document.getElementById('text1');
+  var text2 = document.getElementById('text2');
+
+  if(texto1 == "block"){
+    text2 = "none";
+  }
+}
